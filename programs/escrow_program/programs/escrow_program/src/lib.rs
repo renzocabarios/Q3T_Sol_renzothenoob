ee558d662fe1d6d960d61012852abd3454f459e9
renzocabarios <renzo.cabarios@gmail.com>
2024-08-15 04:33:06 +0800

    Add take

--- programs/escrow_program/programs/escrow_program/src/lib.rs
+++ programs/escrow_program/programs/escrow_program/src/lib.rs
@@ -21,6 +21,11 @@ pub mod escrow_program {
         ctx.accounts.deposit(deposit)?;
         ctx.accounts.save_escrow(seed, receive, &ctx.bumps)
     }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
+    }
 }
 
 #[derive(Accounts)]
