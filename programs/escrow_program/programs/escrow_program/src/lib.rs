84169f9774e68760e5e6078282d7b12a9bd0dcd4
renzocabarios <renzo.cabarios@gmail.com>
2024-08-14 08:32:03 +0800

    Add make

--- programs/escrow_program/programs/escrow_program/src/lib.rs
+++ programs/escrow_program/programs/escrow_program/src/lib.rs
@@ -5,6 +5,9 @@ declare_id!("TvbUu14ofv5yuvgpi35AMrPBWQiHfeG2VgcDnLFB3hw");
 pub mod state;
 pub use state::*;
 
+pub mod contexts;
+pub use contexts::*;
+
 #[program]
 pub mod escrow_program {
     use super::*;
@@ -12,6 +15,11 @@ pub mod escrow_program {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
+    }
+
+    pub fn make(ctx: Context<Make>, seed: u64, receive: u64, deposit: u64) -> Result<()> {
+        ctx.accounts.deposit(deposit)?;
+        ctx.accounts.save_escrow(seed, receive, &ctx.bumps)
     }
 }
 
